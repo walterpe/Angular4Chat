2a437fc6a83127391290b09922d741627cfdaa12
Alban <alban.pipon@gmail.com>
2017-06-14 11:35:45 +0200

    Add interaction, new messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,135 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  private connected:boolean = false;
+  // Inputs
+  name: string = '';
+  text: string = '';
 
-  private users: Array<String> = ["Jean", "Ulises", "Sebastien"]
+  // Display
+  connected: boolean = false;
+  users: string[] = ['Jean', 'Ulises', 'Sebastien'];
 
-  private messages: Array<any> = [
-    {
-      "time": "15:45:28",
-      "author": "Ulises",
-      "text": "Cras ac tellus quis purus cursus mattis a vitae eros. Morbi tempus ligula vitae placerat suscipit."
-    },
-    {
-      "time": "15:45:34",
-      "author": "Sebastien",
-      "text": "Vivamus tristique leo eu ligula lobortis, eget interdum dolor elementum."
-    },
-    {
-      "time": "15:45:40",
-      "author": "Ulises",
-      "text": "Vivamus elementum neque ac pretium imperdiet."
-    },
-    {
-      "time": "15:45:48",
-      "author": "Sebastien",
-      "text": "Aenean sed sapien facilisis, iaculis magna ut, interdum urna."
-    },
-    {
-      "time": "15:45:51",
-      "author": "Jean",
-      "text": "Donec eu purus sit amet magna tempor laoreet. Vivamus egestas nunc non dolor dignissim mattis. Nam quis lacus id est tempus maximus non non diam."
-    },
-    {
-      "time": "15:45:58",
-      "author": "Ulises",
-      "text": "Morbi vehicula nisl a interdum pretium."
-    },
-    {
-      "time": "15:46:11",
-      "author": "Sebastien",
-      "text": "Aliquam sollicitudin mi sed tellus ultricies, sed condimentum ligula consequat."
-    },
-    {
-      "time": "15:47:21",
-      "author": "Jean",
-      "text": "Donec tristique risus lacinia vestibulum accumsan."
-    },
-    {
-      "time": "15:48:18",
-      "author": "Jean",
-      "text": "Cras laoreet ex et porttitor malesuada."
-    },
-    {
-      "time": "15:48:24",
-      "author": "Sebastien",
-      "text": "Donec in"
-    },
-    {
-      "time": "15:48:32",
-      "author": "Jean",
-      "text": "Nam fermentum nisi at ipsum commodo finibus sit amet ut risus."
-    },
-    {
-      "time": "15:48:40",
-      "author": "Ulises",
-      "text": "Phasellus fermentum ex ac ipsum mollis, eget pellentesque ex dictum."
-    },
-    {
-      "time": "15:48:55",
-      "author": "Ulises",
-      "text": "Aliquam vitae magna quis ante tincidunt consequat."
-    },
-    {
-      "time": "15:49:34",
-      "author": "Sebastien",
-      "text": "Donec in"
-    },
-    {
-      "time": "15:50:21",
-      "author": "Sebastien",
-      "text": "Suspendisse quis enim in metus volutpat hendrerit."
-    },
-    {
-      "time": "15:50:22",
-      "author": "Ulises",
-      "text": "Nulla auctor lectus vitae ornare rhoncus."
-    },
-    {
-      "time": "15:50:30",
-      "author": "Sebastien",
-      "text": "Phasellus fermentum ex ac ipsum mollis, eget pellentesque ex dictum."
-    },
-    {
-      "time": "15:50:31",
-      "author": "Jean",
-      "text": "Pellentesque nec sapien eget leo accumsan fringilla. Donec at ante quis purus mollis commodo sit amet non est."
-    },
-    {
-      "time": "15:50:41",
-      "author": "Ulises",
-      "text": "Proin hendrerit elit sit amet nibh hendrerit lobortis."
-    },
-    {
-      "time": "15:50:47",
-      "author": "Sebastien",
-      "text": "Curabitur fermentum lacus at nunc vestibulum placerat. Mauris at arcu pharetra, ullamcorper turpis vitae, varius justo."
-    },
-    {
-      "time": "15:50:50",
-      "author": "Jean",
-      "text": "Nam fermentum nisi at ipsum commodo finibus sit amet ut risus."
-    },
-    {
-      "time": "15:50:56",
-      "author": "Jean",
-      "text": "Aliquam vitae magna quis ante tincidunt consequat."
-    },
-    {
-      "time": "15:51:12",
-      "author": "Sebastien",
-      "text": "Donec in risus eget lectus blandit condimentum."
-    },
-    {
-      "time": "15:51:16",
-      "text": "Duis posuere neque ut lectus viverra ullamcorper et ut orci."
-    },
-    {
-      "time": "15:52:18",
-      "author": "Ulises",
-      "text": "Pellentesque nec sapien eget leo accumsan fringilla. Donec at ante quis purus mollis commodo sit amet non est."
-    }
-  ];
+  /* Expected format of items :
+   {
+   "time": "15:45:28",
+   "author": "Ulises",
+   "text": "Cras ac tellus."
+   }
+   */
+  messages: Array<any> = [];
 
+  connect = function () {
+    this.connected = true;
+    this.users.push(this.name);
+  }
+
+  send = function () {
+    let message: any = {
+      time: this.formatDate(new Date()),
+      author: this.name,
+      text: this.text
+    };
+    this.text = '';
+    this.messages.push(message);
+  }
+
+  private formatDate = function (date: Date) {
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  }
 }
